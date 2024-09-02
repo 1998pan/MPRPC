88b3b90712454b427861a00490203fee21470004
primes <primes@localhost.localdomain>
2024-09-02 02:15:01 -0700

    MPRPC SERVER

---
+++ src/include/mprpccontroller.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <google/protobuf/service.h>
+#include <string>
+
+class MprpcController : public google::protobuf::RpcController
+{
+public:
+    MprpcController();
+    void Reset();
+    bool Failed() const;
+    void SetFailed(const std::string& reason);
+    std::string ErrorText() const;
+
+    //目前未实现的功能
+    void StartCancel();
+    bool IsCanceled() const;
+    void NotifyOnCancel(google::protobuf::Closure* callback);
+private:
+    bool m_failed; // RPC方法执行过程中的状态
+    std::string m_errText; //Rpc方法执行过程中的错误信息
+};
